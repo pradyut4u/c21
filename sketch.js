e7d84276f70ced019c61a980c5057e97bba5305a
pradyut4u <60035671+pradyut4u@users.noreply.github.com>
2020-03-14 22:24:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var sun,earth;
+function setup() {
+  createCanvas(800,800);
+  sun=createSprite(400, 400, 50, 50);
+  earth=createSprite(200, 390, 40, 40);  
+}
+
+function draw() {
+  background(0,0,0); 
+   if(sun.y>earth.y){
+    earth.velocityX=2
+    earth.velocityY=-2
+  } 
+  if(sun.x<earth.x){
+    earth.velocityX=2
+    earth.velocityY=2
+  }
+  if(sun.y<earth.y){
+    earth.velocityX=-2
+    earth.velocityY=2
+  } 
+  if(sun.x>earth.x && sun.y<earth.y){
+    earth.velocityX=-2
+    earth.velocityY=-2
+  }
+  drawSprites();
+}
